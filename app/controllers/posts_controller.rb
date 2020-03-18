9ef14651db064e07d0fdeb46ac876c1a9320823f
Markus Mengel <markus.mengelhx@gmail.com>
2020-03-18 14:21:54 +0100

    fixed user_id was missing

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,6 +10,7 @@ class PostsController < ApplicationController
 
   def create
     @post = Post.new(post_params)
+    @post.user_id = current_user.id
     if @post.save
       redirect_to posts_path
     end
@@ -18,6 +19,6 @@ class PostsController < ApplicationController
   private
 
   def post_params
-    params.require(:post).permit(:title, :content, :tag_id)
+    params.require(:post).permit(:title, :content, :tag_id, :user_id)
   end
 end
