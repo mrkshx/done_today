73d91f14ca13674e1cf42df50befcd3b992dae09
Markus Mengel <markus.mengelhx@gmail.com>
2020-03-18 20:00:22 +0100

    db migrate

---
+++ db/migrate/20200318184314_create_posttags.rb
@@ -0,0 +1,10 @@
+class CreatePosttags < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posttags do |t|
+      t.references :post, foreign_key: true
+      t.references :tag, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_18_115405) do
+ActiveRecord::Schema.define(version: 2020_03_18_184708) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,12 +18,19 @@ ActiveRecord::Schema.define(version: 2020_03_18_115405) do
   create_table "posts", force: :cascade do |t|
     t.string "content"
     t.string "title"
-    t.bigint "tag_id"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["tag_id"], name: "index_posts_on_tag_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "posttags", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "tag_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_posttags_on_post_id"
+    t.index ["tag_id"], name: "index_posttags_on_tag_id"
   end
 
   create_table "tags", force: :cascade do |t|
@@ -44,6 +51,7 @@ ActiveRecord::Schema.define(version: 2020_03_18_115405) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "posts", "tags"
   add_foreign_key "posts", "users"
+  add_foreign_key "posttags", "posts"
+  add_foreign_key "posttags", "tags"
 end
